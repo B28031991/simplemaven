c8825535fd18bbfe2be1c5b508ca4891ba6068c9
Manikandan Renga <Manikandan.Renga@actian.com>
2023-10-25 13:16:07 +0530

    adding as junit testcase

--- src/test/java/test_mavenTests/JavaVersionTest.java
+++ src/test/java/test_mavenTests/JavaVersionTest.java
@@ -1,9 +1,11 @@
 package test_mavenTests;
 
+import org.junit.Test;
 
 public class JavaVersionTest {
 
-	public static void main(String args[]) {
+	@Test
+	public void main() {
 		System.out.println("Hi from Maven project");
 	}
 	
